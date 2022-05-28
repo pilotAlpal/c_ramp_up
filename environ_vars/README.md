6c87d1fac01c290bfca42cda4b8cee766035bb22
LUCAS SEGARRA FERNANDEZ <lsegar01@ucm.es>
2022-05-28 14:13:23 +0200

    Included program to print environment vars

---
+++ environ_vars/README.md
@@ -0,0 +1,9 @@
+# print_env
+
+Program to print environment vars
+
+## Usage
+
+```bash
+./print_env
+```
---
+++ environ_vars/print_env.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+
+extern char** environ;
+
+int main()
+{
+    char** var;
+    for (var = environ; *var != NULL; ++var)
+        printf("%s\n", *var);
+    return 0;
+}
