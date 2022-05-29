6245ce2e152522beb1e533d2da7d6fba420b9a7e
LUCAS SEGARRA FERNANDEZ <lsegar01@ucm.es>
2022-05-29 14:39:29 +0200

    Included program to (inefficiently) calculate the Nth prime number

---
+++ compute_prime/README.md
@@ -0,0 +1,13 @@
+# compute_prime
+
+Calculate (inefficiently) the Nth prime number. N is expected to be provided as program argument.
+
+## Usage Example
+
+> Get the 5000th prime
+
+```bash
+./compute_prime 5000
+```
+
+
---
+++ compute_prime/compute_prime.c
@@ -0,0 +1,38 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void* compute_prime(void* arg) {
+    int candidate = 2;
+    int n = *((int*) arg);
+    while (1) {
+        int factor;
+        int is_prime = 1;
+
+        for (factor = 2; factor < candidate; factor++) {
+            if (candidate % factor == 0) {
+                is_prime = 0;
+                break;
+            }
+        }
+        if (is_prime) {
+            if (--n == 0)
+                return (void *) candidate;
+        }
+        ++candidate;
+    }
+    return NULL;
+}
+
+int main(int argc, char** argv)
+{
+    pthread_t thread;
+    int which_prime = atoi(argv[1]);
+    int prime;
+    pthread_create(&thread, NULL, &compute_prime, &which_prime);
+
+    pthread_join(thread, (void*) &prime);
+    printf("%dth prime is %d\n", which_prime, prime);
+    return 0;
+}
+
